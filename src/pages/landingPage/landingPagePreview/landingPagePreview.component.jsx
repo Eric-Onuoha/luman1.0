6fe00eb60014e6026046d1353e5323143fcd30fe
Eric Onuoha <plipsd@gmail.com>
2023-06-02 14:51:35 +0100

    Design landing page

--- src/pages/landingPage/landingPagePreview/landingPagePreview.component.jsx
+++ src/pages/landingPage/landingPagePreview/landingPagePreview.component.jsx
@@ -4,11 +4,16 @@ import "./landingPagePreview.styles.scss";
 import Navigation from "../../../components/navigation/navigation.component";
 import ContactDetails from "../../../components/contactDetails/contactDetails.component";
 
+import BreadBanner from "../../../assets/images/breadBanner.png";
+import Logo from "../../../assets/images/LushOvensLogo.png";
+
 const LandingPagePreview = () => {
     return(
         <div id="landingPagePreviewComponent">
+            <img src={Logo} alt="" />
 
             <div id="landingIntro">
+                <img id="banner" src={BreadBanner} alt="An Image of Sliced Bread" />
                 <div id="navigationComponent">
                     <Navigation></Navigation>
                 </div>
