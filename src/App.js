69d51411f40bb3932d6791f436f59bf420877235
Eric Onuoha <plipsd@gmail.com>
2023-06-03 20:17:27 +0100

    Landing page laptop view

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 import LandingPage from './pages/landingPage/landingPage.component';
 import Careers from './components/careers/careers.component';
 import ApplicationPage from './pages/applicationPage/applicationPage.component';
+import ContactDetailsPreview from './components/contactDetails/contatcDetailsPreview/contactDetailsPreview.component';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<LandingPage/>} />
+        <Route path='/' element={[<LandingPage/>, <ContactDetailsPreview/>]}/>
         <Route path='/careers' element={<Careers/>} />
         <Route path='/careers/:position' element={<ApplicationPage/>}/>
       </Routes>
--- src/pages/landingPage/landingPagePreview/landingPagePreview.component.jsx
+++ src/pages/landingPage/landingPagePreview/landingPagePreview.component.jsx
@@ -10,19 +10,14 @@ import BreadBanner from "../../../assets/images/breadBanner.png";
 const LandingPagePreview = () => {
     return(
         <div id="landingPagePreviewComponent">
-                <div id="navigationComponent">
-                    <Navigation></Navigation>
-                </div>            
+            <div id="navigationComponent">
+                <Navigation></Navigation>
+            </div>            
 
             <div id="landingIntro">
                 <img id="banner" src={BreadBanner} alt="An Image of Sliced Bread" />
                 <h1 id="brandSlogan">Breakfast made easy!</h1>
             </div>
-
-            <div id="contactDetailsComponent">
-                <ContactDetails></ContactDetails>
-            </div>
-
         </div>
     )
 };
