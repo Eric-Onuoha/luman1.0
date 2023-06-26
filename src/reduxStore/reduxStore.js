54cdfdb746b2d523a1d9d0f804093c919923ce31
Eric Onuoha <plipsd@gmail.com>
2023-06-26 16:48:10 +0100

    remove looger from production

--- src/reduxStore/reduxStore.js
+++ src/reduxStore/reduxStore.js
@@ -1,9 +1,24 @@
-import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+import { compose, legacy_createStore, applyMiddleware } from "redux";
+import logger from "redux-logger"; 
 import { rootReducer } from "./rootReducer";
 
+// const loggerMiddleware = (store) => (next) => (action) =>{ // customized loogerMiddleware
+//     if (!action.type){
+//         return next(action);
+//     }
 
-const middlewares = [logger];
+//     console.log("type", action.type);
+//     console.log("payload", action.payload);
+//     console.log("CurrentState: ", store.getState());
+
+//     next(action);// next is synchorous and would need to complete before anyother code runs
+
+//     console.log("Next State: ", store.getState());
+// }
+
+const middlewares = [process.env.NODE_ENV === "development" && logger].filter(
+    Boolean
+);
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
-export const reduxStore = createStore(rootReducer, undefined, composedEnhancers);
+export const reduxStore = legacy_createStore(rootReducer, undefined, composedEnhancers);
