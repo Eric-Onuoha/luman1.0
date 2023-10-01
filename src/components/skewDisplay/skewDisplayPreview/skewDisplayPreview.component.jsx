843a3da5bc6881764b7526e07e2e7196371b43a3
Eric Onuoha <plipsd@gmail.com>
2023-10-01 15:07:05 +0100

    Update Targets Page to include expense list by category 1

--- src/components/skewDisplay/skewDisplayPreview/skewDisplayPreview.component.jsx
+++ src/components/skewDisplay/skewDisplayPreview/skewDisplayPreview.component.jsx
@@ -1,19 +1,52 @@
 import "./skewDisplayPreview.styles.scss";
 import { Col } from "bootstrap-4-react/lib/components/layout";
+import Table from "bootstrap-4-react/lib/components/table";
 
 const SkewDisplayPreview = ({heading, indicator, skewData}) => {
 
-    const expenseCategory = Object.keys(skewData).filter(element => element !== "ingredient");
+    const expenseCategory = Object.keys(skewData).filter(element => element !== "ingredient" && element !== "salary");
+
+    const expenseBudget = {
+        "dataBundle": 6000,
+        "miscellaneous": 140000,
+        "TopesExpense": 120000,
+        "nylonBag": 7000,
+        "diesel": 125000,
+        "gas": 90000,
+        "lightbill": 15000
+    }
+
     return(
         <Col className ="dashboardDisplay">
             <p className={indicator}></p>
             <p className="displayHeader">{heading}</p>
             <div id="skews">
-            {
-                expenseCategory.map((skew) => (
-                    <p><span className="category">{skew}</span> - {skewData[skew].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                ))
-            }
+                    <Table bordered hover responsive className = "bg-light"> 
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Category</th>
+                                <th>Amount Spent</th>
+                                <th>Budget</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {expenseCategory.map((skew, i) => (
+                            <>
+                                <tr>
+                                <td>{i+1}</td>
+                                <td className="category">{skew}</td>
+                                <td>{skewData[skew].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
+                                {(expenseBudget[skew] > skewData[skew]) ? (
+                                    <td id={"indicatorGood"}>{expenseBudget[skew]}</td>
+                                ) : (
+                                    <td id={"indicatorBad"}>{expenseBudget[skew]}</td>
+                                )}
+                                </tr>
+                            </>
+                        ))}
+                        </tbody>
+                    </Table>
              </div>
         </Col>
     )
--- src/components/targets/targetsPreview/targetsPreview.component.jsx
+++ src/components/targets/targetsPreview/targetsPreview.component.jsx
@@ -1,6 +1,7 @@
 import "./targetsPreview.styles.scss";
 import { Container, Row, Col } from "bootstrap-4-react/lib/components/layout";
 import { GetMonthlyBagPerDay, GetMonthlyDTCSales, GetMonthlySRSales } from "../../../utils/getMonthlyTargets";
+import { Fragment } from "react";
 
 import SingleDisplay from "../../singleDisplay/singleDisplay.component";
 import SkewDisplay from "../../skewDisplay/skewDisplay.component";
@@ -23,29 +24,71 @@ const TargetsPreview = () => {
     const hightestDebtTarget = 60000;
 
     const categoryExpenses = GetExpenseByCategory();
-    // console.log(categoryExpenses);
 
     return(
         <Container id="targetsPreviewComponent" fluid="true">
             {/* <OperationsMenu></OperationsMenu> */}
             <Row id="weeklyTargets">
-                <SingleDisplay indicator={bagsPerDay.bagsperday - targetBagsPerDay} heading = {"Current Bag(s) Per Day"} data={bagsPerDay.bagsperday.toFixed(2) + " of " + targetBagsPerDay.toFixed(2)}></SingleDisplay>
-                <SingleDisplay indicator={(monthDTC.salesdtc.family/bagsPerDay.activeDays) - monthDTCTarget} heading = {"Average DTC Sales Per Day"} data={(monthDTC.salesdtc.family/bagsPerDay.activeDays).toFixed(0) + ` of ${monthDTCTarget.toFixed(0)} / day`}></SingleDisplay>
-                <SingleDisplay indicator={hightestDebtTarget + totalDebt} heading = {"Total Company Debt" + ` (Target - ${hightestDebtTarget.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")})`} data={totalDebt.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}></SingleDisplay>
+                {bagsPerDay.bagsperday.length != 0 ? (
+                    <SingleDisplay indicator={bagsPerDay.bagsperday - targetBagsPerDay} heading = {"Current Bag(s) Per Day"} data={bagsPerDay.bagsperday.toFixed(2) + " of " + targetBagsPerDay.toFixed(2)}></SingleDisplay>
+                ) : (
+                    <SingleDisplay indicator={-10} heading={"Current Bag(s) Per Day"} data={"loading ..."}></SingleDisplay>
+                )}
+                
+                {monthDTC.salesdtc.family ? (
+                    <>
+                    {isFinite((monthDTC.salesdtc.family/bagsPerDay.activeDays)) ? (
+                            <SingleDisplay indicator={(monthDTC.salesdtc.family/bagsPerDay.activeDays) - monthDTCTarget} heading = {"Average DTC Sales Per Day"} data={(monthDTC.salesdtc.family/bagsPerDay.activeDays).toFixed(0) + ` of ${monthDTCTarget.toFixed(0)} / day`}></SingleDisplay>
+                        ) : (
+                            <SingleDisplay indicator={ - monthDTCTarget} heading = {"Average DTC Sales Per Day"} data={0 + ` of ${monthDTCTarget.toFixed(0)} / day`}></SingleDisplay>
+                        )}
+                    </>
+                ) : (
+                    <SingleDisplay indicator={-10} heading={"Average DTC Sales Per Day"} data={"loading ..."}></SingleDisplay>
+                )}
+
+                {totalDebt ? (
+                    <SingleDisplay indicator={hightestDebtTarget + totalDebt} heading = {"Total Company Debt" + ` (Target - ${hightestDebtTarget.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")})`} data={totalDebt.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}></SingleDisplay>
+                ) : (
+                    <SingleDisplay indicator={-10} heading={"Total Company Debt" + ` (Target - ${hightestDebtTarget.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")})`} data={"loading..."}></SingleDisplay>
+                )}
             </Row>
 
             <h4>Sales Rep Average Sales Per Day</h4>
             <Row id="weeklyTargets">
-                {    Object.keys(monthSR).map((salesRep) => (
-                        <SingleDisplay key={salesRep} indicator={(monthSR[salesRep]["family"]/bagsPerDay.activeDays) - averageSRSalesTarget} heading = {salesRep.replace("_", " ")} data={(monthSR[salesRep]["family"]/bagsPerDay.activeDays).toFixed(0) +  ` of ${averageSRSalesTarget.toFixed(0)} / day`}></SingleDisplay>
-                    ))
-                }
+                {Object.keys(monthSR).length !== 0 ? (
+                    <>
+                        {   Object.keys(monthSR).map((salesRep) => (
+                            <>
+                                {isFinite(monthSR[salesRep]["family"]/bagsPerDay.activeDays) ? (
+                                    <>
+                                    <SingleDisplay key={salesRep} indicator={(monthSR[salesRep]["family"]/bagsPerDay.activeDays) - averageSRSalesTarget} heading = {salesRep.replace("_", " ")} data={(monthSR[salesRep]["family"]/bagsPerDay.activeDays).toFixed(0) +  ` of ${averageSRSalesTarget.toFixed(0)} / day`}></SingleDisplay>
+                                    </>
+                                ) : (
+                                    <>
+                                    <SingleDisplay key={salesRep} indicator={ - averageSRSalesTarget} heading = {salesRep.replace("_", " ")} data={0 +  ` of ${averageSRSalesTarget.toFixed(0)} / day`}></SingleDisplay>
+                                    </>
+                                )}
+                            </>
+                            ))
+                        }
+                    </>
+                ) : (
+                    <>
+                        <SingleDisplay indicator={-10} data={"loading ..."}></SingleDisplay>
+                    </>
+                )}
+
             </Row>
 
             
             <h4>Expense List By Category</h4>
             <Row id="debtTarget">
-                <SkewDisplay indicator={"noIndicator"} skewData={categoryExpenses}></SkewDisplay>
+                {Object.keys(categoryExpenses).length !== 0 ? (
+                    <SkewDisplay indicator={"noIndicator"} skewData={categoryExpenses}></SkewDisplay>
+                ) : (
+                    <SingleDisplay indicator={-10} data={"loading ..."}></SingleDisplay>
+                )}
             </Row>
         </Container>
     )
