954a402dc39ee571c64cf421894c7a15b1f4292c
Eric Onuoha <plipsd@gmail.com>
2023-12-07 14:25:40 +0100

    Update active days calculation 1

--- src/utils/getMonthlyTargets.jsx
+++ src/utils/getMonthlyTargets.jsx
@@ -102,7 +102,7 @@ export const GetActiveDays = (currentMonth = month) => {
 
 export const GetMonthlyBagPerDay = (currentMonth = month) => {
     const StockRecords = GetStockRecords();
-    let activeDays = (GetActiveDays() - 1);
+    let activeDays = GetActiveDays();
 
     let totalFamily = 0;
     let totalMini = 0;
