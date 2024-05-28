a42c8aef5ae0cef0f95af1019265f3450a6c3778
Eric Onuoha <plipsd@gmail.com>
2024-05-28 17:05:52 +0100

    moved date to 27th

--- src/utils/getMonthAndDay.jsx
+++ src/utils/getMonthAndDay.jsx
@@ -22,7 +22,7 @@ export const getPlainDate = (dateToChange) => {
 
 export const getCurrentDateToUpdate = () => {
     // This determines what day you are making changes to on LUMAN App
-    return new Date("2024-5-27");
+    return new Date("2024-5-28");
     // return new Date();
 }
 
